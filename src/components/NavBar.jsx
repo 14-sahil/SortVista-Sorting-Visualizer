6efb453833ee4e4f32e381fa47375744940ff5eb
Sahilkaramkar <126070964+14-sahil@users.noreply.github.com>
2024-03-29 02:22:22 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -41,7 +41,7 @@ export function NavBar() {
         }}
       >
         <h3>Sorting Algorithms Visualizer</h3>
-        <a href=" https://github.com/suraj1849/SortingAlgo_Visualizer" target="_blank"><AiFillGithub style={{ fontSize: "1.5rem" }} /></a>
+        <a href="" target="_blank"><AiFillGithub style={{ fontSize: "1.5rem" }} /></a>
       </div>
       <AppBar position="static" color="default">
         <Tabs
