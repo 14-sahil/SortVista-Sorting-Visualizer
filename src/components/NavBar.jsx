26d757445f8472f7cab8415b4c473f1e3b3d122b
Sahilkaramkar <126070964+14-sahil@users.noreply.github.com>
2024-03-29 03:19:16 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -41,7 +41,7 @@ export function NavBar() {
         }}
       >
         <h3>Sorting Algorithms Visualizer</h3>
-        <a href="https://github.com/14-sahil/SortVista-Sorting-Visualizer/tree/master" target="_blank"><AiFillGithub style={{ fontSize: "1.5rem" }} /></a>
+        <a href="https://github.com/14-sahil/SortVista-Sorting-Visualizer" target="_blank"><AiFillGithub style={{ fontSize: "1.5rem" }} /></a>
       </div>
       <AppBar position="static" color="default">
         <Tabs
